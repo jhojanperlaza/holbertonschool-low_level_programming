499853744392bc4c6875946876838d0cdcda005f
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-11 14:23:12 -0800

    alphabet with for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a;
+	char A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	putchar (a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+	putchar (A);
+	putchar ('\n');
+	}
+	return (0);
+}
