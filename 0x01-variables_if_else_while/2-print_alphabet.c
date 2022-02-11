0dd80853bc8d28d561d2fbccec2344877745efce
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-11 13:46:54 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 26;
+	char letra = 'a';
+
+	do {
+	printf("%c/n", letra);
+	letra++;
+	x--;
+	} while (x != 0);
+
+	return (0);
+}
