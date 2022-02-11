8796b4f60ce06b8c6a19e0e9a6c58d36e561ede0
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-10 17:22:01 -0800

    mi first scrip of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char nombre[] = {"\"Programming is like building a multilingual puzzle\n"};
+
+	puts(nombre);
+	return (0);
+}
