d164020876cb9f85786105b0540d607719c94d2c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-11 14:41:17 -0800

    alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	if (a != 'e' && a != 'q')
+	{
+	putchar(a);
+	}
+	}
+	putchar ('\n');
+
+	return (0);
+}
