53742f8a01458e72cda40827fbf8534440bbb975
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-11 15:00:57 -0800

    list from 1 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '1'; num <= '9'; num++)
+	{
+	putchar (num);
+	}
+	return (0);
+}
