a0a60dfd85f721beea26285ad943f8b8dc7ab340
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-10 18:31:51 -0800

    command sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int typelonglong;
 	float typelong;
 
-	printf("Size of a char: %zu bytes\n", sizeof(typechar));
-	printf("Size of an int: %zu bytes\n", sizeof(typeint));
-	printf("Size of a long int: %zu bytes\n", sizeof(typelongint));
-	printf("Size of a long long int: %zu byte\n", sizeof(typelonglong));
-	printf("Size of a float: %zu byte\n", sizeof(typelong));
+	printf("Size of a char: %zu bytes(s)\n", sizeof(typechar));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(typeint));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(typelongint));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(typelonglong));
+	printf("Size of a float: %zu byte(s)\n", sizeof(typelong));
 	return (0);
 }
