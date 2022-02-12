fbc936947677e671718601c9b75b2ec39a77b27b
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-11 16:39:59 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 	int a;
 
 	for (a = '0'; a <= '9'; a++)
-	putchar(a);
 	{
-	if (a != '9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
