71f811b900b93c3b23dde4fef06691f64ad96b5c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-15 07:16:12 -0800

    the last digit, use de int+ '0' for convertir to char

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+* print_last_digit - is a fuction to prints last digit of a number.
+*@n: letter being tested
+*@digit: letter being tested
+* Return: Always 0 (Success)
+*/
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n < 0)
+	{
+		_putchar(-last + '0');
+		return (-last);
+	}
+	else
+	{
+		_putchar(last + '0');
+		return (last);
+	}
+
+}
