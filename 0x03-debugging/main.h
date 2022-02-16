284aae50b7910eb22fcf80a5fb275e9b4490cb23
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-16 14:50:14 -0500

    git the 0-main and main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-/*
-* File: main.h
-* Auth: Jhojan Perlaza
-* Desc: Header file containing declarations for all functions
-*       used in the 0x03-functions_nested_loops directory.
-*/
 
 void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
