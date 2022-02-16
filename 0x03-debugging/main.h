2ff6c84d683648a3c3c11a88d41830564a88d617
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-16 14:52:49 -0800

    check betty in main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,13 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-/*
-* File: main.h
-* Auth: Jhojan Perlaza
-* Desc: Header file containing declarations for all functions
-*       used in the 0x02-functions_nested_loops directory.
-*/
+
 void positive_or_negative(int i);
 
-
-#endif
+#endif /* MAIN_H */
