4d7dc6daedff2ea91293a1349b54d4b82e69c3b4
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-18 12:21:48 -0500

    prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - Entry point
+*a: is a the iterador
+*i: is a condition
+* Return: Always 0 (Success)
+*/
+void more_numbers(void)
+{
+	int a;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			putchar (a + '0');
+		}
+		putchar ('\n');
+		i++;
+	}
+}
