e70e48dd69c436c6ca49d70460ef64216db9971e
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-18 11:02:21 -0500

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _islower - is a fuction to prints the lower character.
+*@c: letter being tested
+* Return: Always 0 (Success)
+*/
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
