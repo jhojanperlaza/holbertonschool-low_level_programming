b31f98ae830e264db228df7cfa5e64067940bedd
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-18 11:42:28 -0500

    prints numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Adds two integers.
+ * n: The first integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+void print_numbers(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	_putchar(n);
+	}
+}
