8997a68ba92f17702b67a26ab2bab83039cf27a9
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-18 22:21:18 -0500

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,28 @@
  */
 int main(void)
 {
-	int n;
-	
-	for(n=1; n<=100; n++)
+	int num;
+
+	for (num = 1; num <= 100; num++)
 	{
-		if (n%3 == 0 && n%5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (n%3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (n%5 ==0)
-		{
-			printf("Buzz ");
-		}
-		else if (n%3 != 0 && n%5 != 0)
-		{
-			printf("%d ",n);
-		}
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
 	}
+
 	printf("\n");
-	
+
 	return (0);
 }
