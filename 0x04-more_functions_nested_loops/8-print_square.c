1bf5459928eec829bca51db00b4b9716cd2993e1
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-18 19:04:46 -0500

    print one cuadrado square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_square - Entry point
+*@size: is a the iterador
+*f: is a condition
+*c: is a condition
+* Return: Always 0 (Success)
+*/
+void print_square(int size)
+{
+	int f, c;
+	
+	for (f = 1; f <= size; f++)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			_putchar ('#');
+		}
+		_putchar ('\n');
+	}
+}
