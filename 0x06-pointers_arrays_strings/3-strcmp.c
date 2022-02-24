56a6515f320cd5f49eb1c9175b9196900462ca4f
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-24 12:38:35 -0500

    Write a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * *_strcat - this function reset n for the value 98
- * @dest: a char value
- * @src : a char value
+ * *_strcmp - this function reset n for the value 98
+ * @s1: a char value
+ * @s2 : a char value
  * Return: nothing.
  */
-char *_strcat(char *dest, char *src)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
+
 	i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i]) 
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
