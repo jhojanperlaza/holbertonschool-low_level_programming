a98afab1045a8761f0c00ff0967bee497a841dad
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-24 00:42:56 -0500

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncat - this function reset n for the value 98
+ * @dest: a char value
+ * @src : a char value
+ * @n: a integer value
+ * Return: nothing.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int x;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	for (x = 0; x <= j && x < n; x++)
+	{
+		dest[i + n] = src[x];
+	}
+
+	return (dest);
+}
