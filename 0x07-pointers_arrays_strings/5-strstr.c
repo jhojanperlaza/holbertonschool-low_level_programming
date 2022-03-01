7e7ecb48e99facc9518ab4ce50ebbe2fc5fba25c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-02-28 22:27:15 -0500

    Write a function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,8 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i])
+			if (needle[j] == haystack[i] && needle[j + 1] == haystack[i + 1]
+					&& needle[j+2] == haystack[i+2])
 			{
 				return (needle);
 			}
