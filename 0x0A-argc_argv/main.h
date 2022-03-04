9f2352b888bf8a3df170565cd18b3b6aee202dd0
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-04 11:19:04 -0500

    Requirements

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+* File: main.h
+* Auth: Jhojan Perlaza
+* Desc: Header file containing declarations for all functions
+* used in the 0x0A.
+*/
+int _putchar(char c);
+#endif
