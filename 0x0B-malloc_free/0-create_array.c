2bdec0e8e99cbd97c12c967eee27dbf2679181dd
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-07 23:02:22 -0500

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	a = (char *) malloc(size * sizeof(char));
 
-	if(a == 0)
+	if (a == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
