aa681b27acc73cf63c219c6e38816f1b7f03404d
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-07 23:00:00 -0500

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	a = (char *) malloc(size * sizeof(char));
 
+	if(a == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
 	}
 	return (a);
-	free(a);
 }
