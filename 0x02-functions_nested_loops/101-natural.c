bab2b016d8cfd68082cfb8c269287384fbe7d6b5
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-13 18:07:12 -0500

    program that computes and prints the sum of all the multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - is a fuction to prints the alphabet.
+*
+* Return: Answer
+*/
+int main(void)
+{
+	int i, j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			j = j + 1;
+	}
+	printf("%i\n", j);
+}
