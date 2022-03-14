f959608363980f940b81fcf7946b6206c25a1e77
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-14 16:13:49 -0500

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for Poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: this is mu first struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
