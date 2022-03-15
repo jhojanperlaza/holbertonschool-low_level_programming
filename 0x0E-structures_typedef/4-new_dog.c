e93dfc3e5af9309eb9d0ad666acb751654e3b0df
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-14 22:51:03 -0500

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+* _strlen - Write a function that returns the length of a string.
+*@s:is a pointer that contain one char
+* Return: the answer
+*/
+
+int _strlen(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+
+	return (1 + _strlen(s + 1));
+}
+/**
  * new_dog - Write a function that creates a new dog
  * @name: First member
  * @age: Second member
@@ -17,11 +32,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog2 == NULL)
 		return (NULL);
 
-	dog2->name = malloc(strlen(name) + 1);
+	dog2->name = malloc(_strlen(name) + 1);
 	if (dog2 == NULL)
 		return (NULL);
 
-	dog2->owner = malloc(strlen(owner) + 1);
+	dog2->owner = malloc(_strlen(owner) + 1);
 	if (dog2 == NULL)
 		return (NULL);
 
