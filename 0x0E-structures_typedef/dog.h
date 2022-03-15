57e2bd457cf0d1757c87e15646167779c4a63606
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-14 20:15:02 -0500

    Requirements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 /**
  * struct dog - struct for Poppy
  * @name: First member
@@ -16,4 +17,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
