d4c8ca2895dcf610a01396697ef7d6412d6f41b8
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-14 22:50:42 -0500

    creates a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+#include <string.h>
 /**
  * struct dog - struct for Poppy
  * @name: First member
@@ -18,4 +19,5 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
