6001c4bcf06155a21c97c535c7ae99f4b3865e87
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-14 23:07:48 -0500

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,16 +30,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
+	{
+		free(dog2);
 		return (NULL);
-
+	}
 	dog2->name = malloc(_strlen(name) + 1);
-	if (dog2 == NULL)
+	if (dog2->name == NULL)
+	{
+		free(dog2->name);
 		return (NULL);
-
+	}
 	dog2->owner = malloc(_strlen(owner) + 1);
-	if (dog2 == NULL)
+	if (dog2->owner == NULL)
+	{
+		free(dog2->owner);
 		return (NULL);
-
+	}
 	while (name[i] != '\0')
 	{
 		dog2->name[i] = name[i];
