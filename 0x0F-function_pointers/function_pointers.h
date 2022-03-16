b3a7531451d2fa7a32a505148c1da8ef73cab639
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-16 09:11:23 -0500

    Requirements

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUCTION_POINTERS_H
+#define FUCTION_POINTERS_H
+#include <stdio.h>
+/*
+* File: function_pointers.h
+* Auth: Jhojan Perlaza
+* Desc: Header file containing declarations for all functions
+* used in the 0x0F.
+*/
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+#endif
