ddef4e04d0a51ba393f1ff64b1035e36d468ce24
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-17 13:11:03 -0500

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,22 +1,30 @@
 #include "3-calc.h"
+/**
+ * get_op_func - structure with operators and functions
+ * @s:pointer to function
+ * File: 3-calc.h
+ * Auth: Jhojan Perlaza
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x0F.
+ * Return: returns the correct function
+ */
 int (*get_op_func(char *s))(int, int)
 {
-
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    while (ops [i].op != NULL)
-    {
-	    if (strcmp(s, ops[i].op) == 0)
-		    break;
-	    i++;
-    }
-    return (ops[i].f);
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,7 +3,7 @@
  * op_add - this function adds two integers
  * @a: is an integer
  * @b: is an integer
- * Return : a sum of two numbers
+ * Return: a sum of two numbers
  */
 int op_add(int a, int b)
 {
@@ -13,17 +13,17 @@ int op_add(int a, int b)
  * op_sub - this function subtracts two integers
  * @a: is an integer
  * @b: is an integer
- * Return : a sub of two numbers
+ * Return: a sub of two numbers
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - this function subtracts two integers
+ * op_mul - this function multiplies two numbers
  * @a: is an integer
  * @b: is an integer
- * Return : a sub of two numbers
+ * Return: product of two numbers
  */
 
 int op_mul(int a, int b)
@@ -31,32 +31,32 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 /**
- * op_sub - this function subtracts two integers
+ * op_div - this function divides two numbers
  * @a: is an integer
  * @b: is an integer
- * Return : a sub of two numbers
+ * Return: division of two numbers
  */
 int op_div(int a, int b)
 {
 	if (a == 0 || b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
 /**
- * op_sub - this function subtracts two integers
+ * op_mod - this function makes the module of two numbers
  * @a: is an integer
  * @b: is an integer
- * Return : a sub of two numbers
+ * Return: a module of two numbers
  */
 int op_mod(int a, int b)
 {
 	if (a == 0 || b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a % b);
 }
