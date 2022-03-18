9bd947c5392c44e6bb6c588e1b564e34fb14459d
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-18 09:20:26 -0500

    Requirements

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+/*
+* File: variadic_functions.h
+* Auth: Jhojan Perlaza
+* Desc: Header file containing declarations for all functions
+* used in the 0x10.
+*/
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
