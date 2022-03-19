f62e79c1ad7ec0312d7154d8b489b4f7485c7738
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-18 22:33:05 -0500

    prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,19 +10,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list unknown_parameters;
 
-	if (separator == NULL || n == 0)
-		return;
-
 	va_start(unknown_parameters, n);
+
+	if (n == 0)
+		return;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(unknown_parameters, int));
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
-	printf("\n");
 	va_end(unknown_parameters);
+	printf("\n");
 }
