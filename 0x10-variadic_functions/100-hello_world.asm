1765ea7608000b787495f219d9e46677293dae14
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-20 00:10:16 -0500

    program in assembly that prints Hello, World

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,16 +1,19 @@
-section .text
-	global main		;must be declared for linker (gcc)
-main:				;tell linker entry point
-	mov rax, 1		;system call for write
-	mov rdi, 1 		;file handle 1 is stdout
-	mov rsi, msg		;address of string to output
-	mov rdx, 17 		;number of bytes
-	syscall 		;invoke operating system to do the write
-	mov rax, 60 		;system call for exit
-	mov rdi, 0	; exit code 0
-	syscall
+section .data
+	msg db "Hello, World",10 ;our dear string
 
-section .data 			;The data section is used for declaring initialized data or constants.
+	section .text
+	global main
 
-	msg db  'Hello, world',0xa ;our dear string
-	len equ 10		       ;length of our dear string 
+; linker puts the entry point here:
+main:
+; Write the string to stdout:
+	mov rax, 1 ;system call number (sys_write) 
+	mov rdi, 1 ;file descriptor (stdout)
+	mov rsi, msg ;message to write
+	mov rdx, 13 ;message length
+	syscall  ;call kernel
+
+; Exit via the kernel:
+	mov rax, 0 ;process' exit code
+	mov rdi, 0 ;system call number (sys_exit)
+	syscall  ;call kernel - this interrupt won't return
