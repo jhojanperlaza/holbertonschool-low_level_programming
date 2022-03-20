b4f7d0f1eadddaacf9eb63b5d32431f893ae361c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-19 22:26:19 -0500

    program in assembly that prints Hello, World

---
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -0,0 +1,16 @@
+section .text
+	global main		;must be declared for linker (gcc)
+main:				;tell linker entry point
+	mov rax, 1		;system call for write
+	mov rdi, 1 		;file handle 1 is stdout
+	mov rsi, msg		;address of string to output
+	mov rdx, 17 		;number of bytes
+	syscall 		;invoke operating system to do the write
+	mov rax, 60 		;system call for exit
+	mov rdi, 0	; exit code 0
+	syscall
+
+section .data 			;The data section is used for declaring initialized data or constants.
+
+	msg db  'Hello, Holberton',0xa ;our dear string
+	len equ 10		       ;length of our dear string
