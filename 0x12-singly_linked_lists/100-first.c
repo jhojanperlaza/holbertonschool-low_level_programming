13002982d5f8e19a9d69250b2915d284964e8f72
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-29 16:44:26 -0500

    Write a function that prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include "lists.h"
-
-void run_before_main (void)
+/**
+ * run_before_main - Write a function that prints
+ * before the main function is executed
+ * Return: Always 0.
+ */
+void run_before_main(void)
 {
-    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
