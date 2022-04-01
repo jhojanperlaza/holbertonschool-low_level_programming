dc7f56e520ba055332cf33d9e46eff55b1ffc06b
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-01 13:16:15 -0500

    function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_listint2 - function that frees a list
+*@head:is the pointer to head
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *p_aux = *head;
+	listint_t *p_aux2 = NULL;
+
+	while (p_aux != NULL)
+	{
+		p_aux2 = p_aux;
+		p_aux = p_aux->next;
+		free (p_aux2);
+	}
+	*head =NULL;
+}
