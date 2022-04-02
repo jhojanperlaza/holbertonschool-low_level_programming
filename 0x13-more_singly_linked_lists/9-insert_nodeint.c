bcd96c7a22ae06e84a838896b00988ae13c5329f
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-02 12:45:38 -0500

    correction for the check 4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (p_aux != NULL)
 	{
 		if (cont == idx)
