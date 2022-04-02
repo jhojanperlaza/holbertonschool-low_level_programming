ec8dbbea382c9227ccc4c66230bd09e9231a2560
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-01 20:41:00 -0500

    the first commit for task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+*get_nodeint_at_index - function that returns a specific node
+*@head:is a pointer the singly linked list
+*@index:is the index of the list
+* Return: the nth node of a linked list
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int cont = 0;
+	listint_t *p_aux = head;
+
+	while (p_aux != NULL)
+	{
+		p_aux = p_aux->next;
+		cont++;
+		if (cont == index)
+			return (p_aux);
+	}
+	return (NULL);
+}
