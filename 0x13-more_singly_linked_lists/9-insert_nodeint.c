c206cb204ccde8225d00ed33cae1d867d2ce11f2
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-01 23:23:26 -0500

    the first commit in a function that inserts a new node at a given position, task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+*insert_nodeint_at_index - function that inserts a new node at a given position
+*@head:is a pointer the singly linked list
+*@idx:is the index of the list
+*@n: is the new value
+* Return:the address of the new node
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int cont = 1;
+	listint_t *p_aux = *head;
+	listint_t *new_node = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	while (p_aux != NULL)
+	{
+		if (cont == idx)
+		{
+			new_node->next = p_aux->next;
+			p_aux->next = new_node;
+			return (new_node);
+		}
+		cont++;
+		p_aux = p_aux->next;
+	}
+	if (cont < idx)
+		return (NULL);
+	return (NULL);
+}
