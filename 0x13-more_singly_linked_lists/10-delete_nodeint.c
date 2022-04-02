fc87df8f9d6bc89cfc2211b3d2d1bc2cd62552d6
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-01 23:59:24 -0500

    task10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+*delete_nodeint_at_index - function that returns a specific node
+*@head:is a pointer the singly linked list
+*@index:is the index of the list
+* Return: the nth node of a linked list
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}
