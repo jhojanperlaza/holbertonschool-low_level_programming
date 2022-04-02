4ff65db118f16099b0b2875d223dfe86c580a5e8
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-01 23:37:43 -0500

    checks fails, 6,8,9 change code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *p_aux = *head;
 	listint_t *new_node = NULL;
 
-	if (*head == NULL || !head)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
