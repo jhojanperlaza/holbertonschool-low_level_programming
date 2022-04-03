a353fc62337cbde21c483e16120bd3032e97cd18
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-02 22:43:16 -0500

    first commit for task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,5 +7,29 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int cont = 1;
+	listint_t *p_aux = *head;
 
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		p_aux->next = NULL;
+		free(p_aux);
+		return (1);
+	}
+	while (p_aux != NULL)
+	{
+		if (cont == index)
+		{
+			listint_t *del = p_aux->next;
+
+			p_aux->next = p_aux->next->next;
+			del->next = NULL;
+			free(del);
+			return (1);
+		}
+		cont++;
+		p_aux = p_aux->next;
+	}
+	return (-1);
 }
