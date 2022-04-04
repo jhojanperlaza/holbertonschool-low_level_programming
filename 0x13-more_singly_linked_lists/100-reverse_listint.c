cdff36e091735a6fd744ecfeb9a8c1cb157f0c19
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-03 23:47:41 -0500

    add contion for task 8 and 9

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,6 +6,10 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
+	if ((*head)->next == NULL)
+		return (*head);
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	listint_t *p_prev = (*head)->next;
 	listint_t *p_next = p_prev->next;
 
