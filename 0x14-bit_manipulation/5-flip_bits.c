638cce8c8f6c4fed1b6c16c032ad31113cd70b0f
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-05 20:05:27 -0500

    first push

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* flip_bits - Count number of bits to be flipped
+* @n:first number
+* @m:second number
+* Return: #flips
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flips = 0;
+
+	while (n > 0 || m > 0)
+	{
+		unsigned int t1 = (n & 1);
+		unsigned int t2 = (m & 1);
+
+		if (t1 != t2)
+			flips++;
+		n >> = 1;
+		m >> = 1;
+	}
+	return (flips);
+}
