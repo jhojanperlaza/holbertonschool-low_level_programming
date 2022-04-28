2b9666f657e4eb74f6d8a9dfeb4c4154963893e2
jhojanperlaza <jhojanperlaza@gmail.com>
2022-04-28 11:49:54 -0500

    [add] task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	long int n;
+	long int div = 2, maxFact;
+	n = 612852475143;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%ld\n",maxFact);
+				break;
+			}
+		}
+	}
+	return 0;
+}
