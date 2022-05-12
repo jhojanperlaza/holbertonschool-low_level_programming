deab02dcf9ce9b685054aeae93937e9b6a85ecff
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-12 11:08:48 -0500

    [add] file 7-insert_nodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,22 +9,25 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *p_aux = *h;
+	dlistint_t *p_aux = NULL;
 	unsigned int cont = 0;
 	dlistint_t *new_node = NULL;
 
+	if (!h)
+		return (NULL);
+	p_aux = *h;
 	while (p_aux)
 	{
+		if (idx == 0)
+			return (add_dnodeint(h, n));
 		if (cont + 1 == idx)
 		{
 			new_node = add_dnodeint(&p_aux->next, n);
 			p_aux->next->prev = p_aux;
-			return (&new_node);
+			return (new_node);
 		}
 		cont++;
 		p_aux = p_aux->next;
 	}
-	if (cont < idx)
-		return (NULL);
-	return (&new_node);
+	return (NULL);
 }
