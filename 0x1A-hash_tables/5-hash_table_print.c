75e8364da9c40e37f2b20b78fb785480b08f7124
jhojan <jhojan_perlaza123>
2022-06-06 10:47:13 -0500

    [ADD] file 5-hash

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: is the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	unsigned long int last_index = 0;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+			last_index = i;
+		i++;
+	}
+	if (last_index == 0)
+	{
+		printf("{ }\n");
+		return;
+	}
+	i = 0;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL && i != last_index)
+		{
+			printf("\'%s\': \'%s\', ", ht->array[i]->key, ht->array[i]->value);
+		}
+		else if (i == last_index)
+		{
+			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
+		}
+		i++;
+	}
+	printf("}\n");
+}
