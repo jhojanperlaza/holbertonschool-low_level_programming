5a320a455270a209887d766922801cf7de4a70ac
jhojan <jhojan_perlaza123>
2022-06-05 22:47:58 -0500

    [UPDATED] 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index_hash;
 	hash_node_t *new_node;
 
-	if (!key || !*key) 
+	if (!key)
+		return (0);
+	if (!*key)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
