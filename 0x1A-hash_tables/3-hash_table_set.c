24921fb6846cc01f9cf276420c589eef8cbe73df
jhojan <jhojan_perlaza123>
2022-06-05 23:19:59 -0500

    [UPDATED] 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (!*key || *key == ' ')
 		return (0);
+	if (!ht)
+		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
