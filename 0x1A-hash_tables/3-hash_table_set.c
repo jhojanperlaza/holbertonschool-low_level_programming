485e1b8a7eab50edbf324f170ba1409d3250c55a
jhojan <jhojan_perlaza123>
2022-06-06 00:23:39 -0500

    [UPDATED] 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,18 +13,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index_hash;
 	hash_node_t *new_node;
+	hash_node_t *p_aux;
 
 	if (!key)
 		return (0);
-	if (!*key || *key == ' ')
-		return (0);
-	if (!ht)
+	if (!*key || *key == ' ' || !ht)
 		return (0);
 
+	index_hash = key_index((const unsigned char *)key, ht->size);
+	p_aux = ht->array[index_hash];
+	while (p_aux != NULL)
+	{
+		if (strcmp(p_aux->key, key) == 0)
+		{
+			free(p_aux->value);
+			p_aux->value = strdup(value);
+			return (1);
+		}
+		p_aux = p_aux->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	index_hash = key_index((const unsigned char *)key, ht->size);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index_hash];
