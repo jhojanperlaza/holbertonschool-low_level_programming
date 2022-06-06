1710e3ebf04f6e4e80e8f8d93fcfd169c8dcd944
jhojan <jhojan_perlaza123>
2022-06-05 23:11:32 -0500

    [UPDATED] 3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index_hash = key_index((const unsigned char *)key, ht->size);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
+	new_node->next = ht->array[index_hash];
 	ht->array[index_hash] = new_node;
 
 	return (1);
