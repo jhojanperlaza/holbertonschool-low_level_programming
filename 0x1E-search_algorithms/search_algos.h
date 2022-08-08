337bcfb088e3bf4eafbb6bbc6f2ec3105351013e
jhojan <jhojan_perlaza123>
2022-08-08 13:35:00 -0500

    [ADD] file header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+/*
+* File: search_algos.h
+* Auth: Jhojan Perlaza
+* Desc: Header file containing declarations for all functions
+* used in the 0x1E.
+*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
